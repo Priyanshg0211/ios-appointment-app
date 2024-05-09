8332b56a9fb81ee9268abafd0555f8741caf403b
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-05-10 00:49:30 +0530

    added onboarding page

---
+++ lib/Screens/Getting Started/Onboarding_1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _OnboardingState extends StatefulWidget {
+  const _OnboardingState({super.key});
+
+  @override
+  State<_OnboardingState> createState() => __OnboardingStateState();
+}
+
+class __OnboardingStateState extends State<_OnboardingState> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
