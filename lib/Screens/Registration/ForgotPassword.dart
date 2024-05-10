fdc56b832d0d53f25952077a25654bf38526b500
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-05-10 14:02:46 +0530

    now user can easily create new password

--- lib/Screens/Registration/ForgotPassword.dart
+++ lib/Screens/Registration/ForgotPassword.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ForgotPassword extends StatefulWidget {
@@ -8,7 +9,30 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
-  TextEditingController _emailController = TextEditingController();
+  String email = "";
+  TextEditingController emailcontroller = new TextEditingController();
+
+  final _formkey = GlobalKey<FormState>();
+
+  resetPassword() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+        "Password Reset Email has been sent !",
+        style: TextStyle(fontSize: 15.0),
+      )));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+          "No user found for that email.",
+          style: TextStyle(fontSize: 15.0),
+        )));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,96 +40,105 @@ class _ForgotPasswordState extends State<ForgotPassword> {
         backgroundColor: Colors.transparent,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 20,top: 30),
-              child: Text(
-                'Forgot Password ?',
-                style: TextStyle(
-                  fontFamily: 'Codenextbold',
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Text(
-                'Enter Your email address',
-                style: TextStyle(
-                    fontFamily: 'Codenext', color: Colors.grey, fontSize: 15),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Container(
-                width: MediaQuery.sizeOf(context).width * 0.9,
-                child: TextFormField(
-                  controller: _emailController,
-                  cursorColor: Color(0xff007B5D),
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Colors.grey,
-                    ),
-                    border: InputBorder.none, // Remove underline
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE7EAEB)),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE7EAEB)),
-                    ),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    if (!value.contains('@')) {
-                      return 'Please enter a valid email';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Center(
-              child: Container(
-                height: 50,
-                width: MediaQuery.sizeOf(context).width * 0.8,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff007B5D),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                  onPressed: () {
-                    print("The Button is Pressed");
-                  },
-                  child: Text(
-                    'Done',
-                    style: TextStyle(
-                      fontFamily: 'Codenextbold',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20, top: 30),
+                child: Text(
+                  'Forgot Password ?',
+                  style: TextStyle(
+                    fontFamily: 'Codenextbold',
+                    fontSize: 24,
                   ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Enter Your email address',
+                  style: TextStyle(
+                      fontFamily: 'Codenext', color: Colors.grey, fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Container(
+                  width: MediaQuery.sizeOf(context).width * 0.9,
+                  child: TextFormField(
+                    controller: emailcontroller,
+                    cursorColor: Color(0xff007B5D),
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.grey,
+                      ),
+                      border: InputBorder.none, // Remove underline
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE7EAEB)),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE7EAEB)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      if (!value.contains('@')) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.sizeOf(context).width * 0.8,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff007B5D),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    onPressed: () {
+                      print("The Button is Pressed");
+                      if (_formkey.currentState!.validate()) {
+                        setState(() {
+                          email = emailcontroller.text;
+                        });
+                        resetPassword();
+                      }
+                    },
+                    child: Text(
+                      'Done',
+                      style: TextStyle(
+                        fontFamily: 'Codenextbold',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
