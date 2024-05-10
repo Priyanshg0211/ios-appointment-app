4418495960067ff20952d091c4359fb40aa03cc5
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-05-10 12:51:55 +0530

    Complete the auth for signup page

--- lib/Screens/Registration/SignUp.dart
+++ lib/Screens/Registration/SignUp.dart
@@ -1,5 +1,8 @@
+import 'package:appointment_app/Screens/Homepage/home.dart';
 import 'package:appointment_app/Screens/Registration/SignIn.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -9,9 +12,45 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  String email = "", password = "", name = "";
+  TextEditingController namecontroller = new TextEditingController();
+  TextEditingController passwordcontroller = new TextEditingController();
+  TextEditingController emailcontroller = new TextEditingController();
+
+  final _formkey = GlobalKey<FormState>();
   bool _obscureText = true;
+  registration() async {
+    if (password != null &&
+        namecontroller.text != "" &&
+        emailcontroller.text != "") {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password);
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+          "Registered Successfully",
+          style: TextStyle(fontSize: 15.0),
+        )));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Home()));
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+                "Password Provided is too Weak",
+                style: TextStyle(fontSize: 15.0),
+              )));
+        } else if (e.code == "email-already-in-use") {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+                "Account Already exists",
+                style: TextStyle(fontSize: 15.0),
+              )));
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,215 +59,243 @@ class _SignUpState extends State<SignUp> {
         automaticallyImplyLeading: false,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Text(
-                'Sign Up',
-                style: TextStyle(
-                  fontFamily: 'Codenextbold',
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Text(
-                'Your Username',
-                style: TextStyle(
-                    fontFamily: 'Codenext', color: Colors.grey, fontSize: 15),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Container(
-                width: MediaQuery.sizeOf(context).width * 0.9,
-                child: TextFormField(
-                  controller: _emailController,
-                  cursorColor: Color(0xff007B5D),
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.account_circle_rounded,
-                      color: Colors.grey,
-                    ),
-                    border: InputBorder.none, // Remove underline
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE7EAEB)),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE7EAEB)),
-                    ),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    fontFamily: 'Codenextbold',
+                    fontSize: 24,
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Text(
-                'Your email address',
-                style: TextStyle(
-                    fontFamily: 'Codenext', color: Colors.grey, fontSize: 15),
+              SizedBox(
+                height: 40,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Container(
-                width: MediaQuery.sizeOf(context).width * 0.9,
-                child: TextFormField(
-                  controller: _emailController,
-                  cursorColor: Color(0xff007B5D),
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Colors.grey,
-                    ),
-                    border: InputBorder.none, // Remove underline
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE7EAEB)),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE7EAEB)),
-                    ),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    if (!value.contains('@')) {
-                      return 'Please enter a valid email';
-                    }
-                    return null;
-                  },
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Name',
+                  style: TextStyle(
+                      fontFamily: 'Codenext', color: Colors.grey, fontSize: 15),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Text(
-                'Password',
-                style: TextStyle(
-                    fontFamily: 'Codenext', color: Colors.grey, fontSize: 15),
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Container(
-                width: MediaQuery.sizeOf(context).width * 0.9,
-                child: TextFormField(
-                  controller: _passwordController,
-                  cursorColor: Color(0xff007B5D),
-                  obscureText: _obscureText,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.grey,
-                    ),
-                    border: OutlineInputBorder(),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE7EAEB)),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xffE7EAEB)),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _obscureText ? Icons.visibility_off : Icons.visibility,
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Container(
+                  width: MediaQuery.sizeOf(context).width * 0.9,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please Enter Name';
+                      }
+                      return null;
+                    },
+                    controller: namecontroller,
+                    cursorColor: Color(0xff007B5D),
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.account_circle_rounded,
                         color: Colors.grey,
                       ),
-                      onPressed: () {
-                        setState(() {
-                          _obscureText = !_obscureText;
-                        });
-                      },
+                      border: InputBorder.none, // Remove underline
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE7EAEB)),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE7EAEB)),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Center(
-              child: Container(
-                height: 50,
-                width: MediaQuery.sizeOf(context).width * 0.8,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff007B5D),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Your email address',
+                  style: TextStyle(
+                      fontFamily: 'Codenext', color: Colors.grey, fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Container(
+                  width: MediaQuery.sizeOf(context).width * 0.9,
+                  child: TextFormField(
+                    controller: emailcontroller,
+                    cursorColor: Color(0xff007B5D),
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.grey,
+                      ),
+                      border: InputBorder.none, // Remove underline
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE7EAEB)),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE7EAEB)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      if (!value.contains('@')) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Password',
+                  style: TextStyle(
+                      fontFamily: 'Codenext', color: Colors.grey, fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Container(
+                  width: MediaQuery.sizeOf(context).width * 0.9,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please Enter Password';
+                      }
+                      return null;
+                    },
+                    controller: passwordcontroller,
+                    cursorColor: Color(0xff007B5D),
+                    obscureText: _obscureText,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.grey,
+                      ),
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE7EAEB)),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xffE7EAEB)),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureText
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                      ),
                     ),
                   ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.sizeOf(context).width * 0.8,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff007B5D),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    onPressed: () {
+                      {
+                        print("The Button is Pressed");
+                        if (_formkey.currentState!.validate()) {
+                          setState(() {
+                            email = emailcontroller.text;
+                            name = namecontroller.text;
+                            password = passwordcontroller.text;
+                          });
+                        }
+                        registration();
+                      }
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontFamily: 'Codenextbold',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 150,
+                ),
+                child: Center(
+                  child: Text(
+                    'Already have an account',
+                    style: TextStyle(
+                        fontFamily: 'Codenextbold',
+                        color: Colors.black,
+                        fontSize: 13),
+                  ),
+                ),
+              ),
+              Center(
+                child: TextButton(
                   onPressed: () {
-                    print("The Button is Pressed");
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignIn()));
                   },
                   child: Text(
-                    'Sign Up',
+                    'Login',
                     style: TextStyle(
-                      fontFamily: 'Codenextbold',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
+                        fontFamily: 'Codenextbold',
+                        color: Color(0xff007B5D),
+                        fontSize: 13),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 150,), 
-              child: Center(
-                child: Text(
-                  'Already have an account',
-                  style: TextStyle(
-                      fontFamily: 'Codenextbold',
-                      color: Colors.black,
-                      fontSize: 13),
-                ),
-              ),
-            ),
-        
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => SignIn()));
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                      fontFamily: 'Codenextbold',
-                      color: Color(0xff007B5D),
-                      fontSize: 13),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
